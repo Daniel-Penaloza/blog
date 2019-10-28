716f37feb8be76c3bbcfc170f846aacde3a82293
Daniel-Penaloza <danielpenalozatj@gmail.com>
2019-10-27 20:30:26 -0700

    Finishing the blog

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :update, :edit, :destroy]
 	def index
 		@posts = Post.all.order("created_at DESC")
 	end
@@ -17,15 +18,12 @@ class PostsController < ApplicationController
 	end	 
 
 	def show
-		@post = Post.find(params[:id])
 	end
 
 	def edit
-		@post = Post.find(params[:id])
 	end
 
 	def update
-		@post = Post.find(params[:id])
 		if @post.update(post_params)
 			redirect_to @post
 		else
@@ -34,14 +32,16 @@ class PostsController < ApplicationController
 	end
 
 	def destroy
-		@post = Post.find(params[:id])
 		@post.destroy
-		
 		redirect_to posts_path
 	end
 
 	private
 		def post_params
 			params.require(:post).permit(:title, :content)
+		end
+
+		def find_post
+			@post = Post.find(params[:id])
 		end
 end
